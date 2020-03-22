b886ddce84d68fe190d188e81669fe518d4336ff
aconsigny <aconsigny@crakmedia.com>
2020-03-22 18:54:04 -0400

    Setup accessibility tab in story

--- src/demo-button/stories/demo-button.stories.js
+++ src/demo-button/stories/demo-button.stories.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { action } from '@storybook/addon-actions';
 import Button from '../components/button';
+import { withA11y } from "@storybook/addon-a11y";
 
 export default {
   title: 'Demo-Button',
   component: Button,
+  decorators: [withA11y]
 };
 
 // Story : Basic button with text
