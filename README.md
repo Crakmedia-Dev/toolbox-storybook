fe9bb0069ba234706b3e83a235e8c5a76c662330
Michelle Rochon <mrochon@crakmedia.com>
2020-03-23 10:20:43 -0400

    Ajout des lint etc dans le projet

--- README.md
+++ README.md
@@ -1,21 +1,29 @@
 ## Toolbox storybook
 
-#### Install storybook dependencies 
+#### Install storybook dependencies
+
 ```
 yarn install
+npm i
 ```
 
 #### Launch storybook
+
 ```
 yarn storybook
+npm run storybook
 ```
 
 #### Build a deployable storybook
+
 ```
 yarn build-storybook
+npm run build-storybook
 ```
 
 #### Launch tests
+
 ```
 yarn test
+npm run test
 ```
---
+++ src/demo-button/stories/demo-button.stories.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import { action } from '@storybook/addon-actions'
+import { withA11y } from '@storybook/addon-a11y'
+import { text, withKnobs } from '@storybook/addon-knobs'
+import Button from '../components/button'
+
+export default {
+  title: 'Demo-Button',
+  component: Button,
+  decorators: [withA11y, withKnobs]
+}
+
+export const WithText = () => (
+  <Button onClick={action('clicked')}>
+    {text('children', 'Hello world!')}
+  </Button>
+)
+
+export const WithEmoji = () => (
+  <Button onClick={action('clicked')}>
+    <span role="img" aria-label="so cool">
+      {text('children', '😀 😎 👍 💯')}
+    </span>
+  </Button>
+)
+
+// Removes doc page for this story
+WithEmoji.story = {
+  parameters: { docs: { page: null } }
+}
