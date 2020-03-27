493077a4878e109d49138c511b4cf0195c14091b
aconsigny <aconsigny@crakmedia.com>
2020-03-27 12:59:36 -0400

    Setup jest addon for whole project

--- src/demo-button/stories/demo-button.stories.jsx
+++ src/demo-button/stories/demo-button.stories.jsx
@@ -2,14 +2,12 @@ import React from 'react'
 import { action } from '@storybook/addon-actions'
 import { withA11y } from '@storybook/addon-a11y'
 import { text, withKnobs } from '@storybook/addon-knobs'
-import { withTests } from '@storybook/addon-jest'
 import Button from '../components/button'
-import results from '../../.jest/test-results.json'
 
 export default {
   title: 'Demo-Button',
   component: Button,
-  decorators: [withA11y, withKnobs, withTests({ results })]
+  decorators: [withA11y, withKnobs]
 }
 
 export const WithText = () => (
